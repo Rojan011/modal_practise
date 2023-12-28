f85365bd79e893941920d3f013bdf1633697ff94
Rojan <shrestharojan79@gmail.com>
2023-12-28 19:53:35 +0545

    fragments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
@@ -10,10 +10,10 @@ const addUserHandler=(uNaME,uAge)=>{
   })
 }
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList}/>
-    </div>
+    </Fragment>
   );
 }
 
